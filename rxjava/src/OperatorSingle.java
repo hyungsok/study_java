9bf86b7afdcddaade4ac0bce11c3ac97061f9f1f
kevin <kevin.lee@mangoplate.com>
2017-11-15 18:46:33 +0900

    rename classes

--- rxjava/src/OperatorSingle.java
+++ rxjava/src/OperatorSingle.java
@@ -2,7 +2,7 @@
  * Created by hyungsoklee on 2017. 11. 15..
  * https://github.com/ReactiveX/RxJava/wiki/What%27s-different-in-2.0#observable-and-flowable
  */
-public class Single {
+public class OperatorSingle {
     public static void main(String[] args) {
 
     }
